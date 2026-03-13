701f235ccc77eb804831d5eb0cb7aa40f5923ffc
Maxim <maxipak@ya.ru>
2026-03-13 20:07:57 +0600

    Initial Commit

---
+++ WeatherTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherTest
+//
+//  Created by Maxim Pakhotin on 13.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
